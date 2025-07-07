167f69b8e39794e2230c613dfdd22183cd5dcb93
Fei Hu <fei.hu@fei.io>
2025-07-07 16:57:20 -0500

    refactor: clean up usused code

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import Layout from './components/Layout/Layout';
 import About from './pages/About';
 import Home from './pages/Day';
 import Loading from './pages/Loading';
-// import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -19,8 +17,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/load" element={<Loading />} />
-            {/* <Route path="/projects" element={<Projects />} /> */}
-            {/* <Route path="/contact" element={<Contact />} /> */}
           </Routes>
         </Layout>
       </Router>
