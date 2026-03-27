bcdb5fc3925fede5af935b10f91198f8455f951f
Fei Hu <fei.hu@fei.io>
2026-03-27 15:27:11 -0500

    feat: add VideoBackground component

---
+++ src/components/Landing/VideoBackground.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  inset: 0;
+  z-index: 0;
+`;
+
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.3);
+`;
+
+interface Props {
+  onCanPlay: () => void;
+}
+
+const VideoBackground: React.FC<Props> = ({ onCanPlay }) => (
+  <Wrapper>
+    <Video
+      src="/intro.mp4"
+      autoPlay
+      loop
+      muted
+      playsInline
+      onCanPlay={onCanPlay}
+    />
+    <Overlay />
+  </Wrapper>
+);
+
+export default VideoBackground;
